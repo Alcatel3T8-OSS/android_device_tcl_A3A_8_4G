332ef6f0d6b4a390b0c3df60860f29f9f5fa62de
rvsmooth <riveeks.smooth@gmail.com>
2025-05-03 12:59:04 +0530

    Initial TWRP Device Tree

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for TCL 9027Q (A3A_8_4G)
+
+```
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_A3A_8_4G.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from A3A_8_4G device
+$(call inherit-product, device/tcl/A3A_8_4G/device.mk)
+
+PRODUCT_DEVICE := A3A_8_4G
+PRODUCT_NAME := omni_A3A_8_4G
+PRODUCT_BRAND := TCL
+PRODUCT_MODEL := 9027Q
+PRODUCT_MANUFACTURER := tcl
+
+PRODUCT_GMS_CLIENTID_BASE := 
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="9027X-user 8.1.0 O11019 vB43-0 release-keys"
+
+BUILD_FINGERPRINT := TCL/9027Q/A3A_8_4G:8.1.0/O11019/vB43-0:user/release-keys
